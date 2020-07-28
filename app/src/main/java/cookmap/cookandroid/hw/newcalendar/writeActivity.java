0d100970e9c722a73f6a3cf342c99d2fd378e8d2
beewoo01 <40164024+beewoo01@users.noreply.github.com>
2020-07-28 23:38:28 +0900

    07/28 write_page.02

--- app/src/main/java/cookmap/cookandroid/hw/newcalendar/writeActivity.java
+++ app/src/main/java/cookmap/cookandroid/hw/newcalendar/writeActivity.java
@@ -2,64 +2,73 @@ package cookmap.cookandroid.hw.newcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
-import androidx.fragment.app.DialogFragment;
 
-import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class writeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView backbtn, checkbtn;
+    ImageView backbtn;
     EditText titleEdit, desEdit;
-    TextView s_date, e_date, s_txt, e_txt;
-    LinearLayout start_pick, end_pick;
-    ImageButton fap;
-    String stime, etime;
+    TextView s_date, e_date, s_txt, e_txt, checkbtn;
+    RelativeLayout select_date, fap;
     String dbName = "con_file.db";
     int dbVersion = 1;
     private SQLiteOpenHelper helper;
     private SQLiteDatabase db;
+    String startDate, endDate;
     String tag = "SQLite";
     String tableName = "contents";
     String des = "", m_img = "" ,img= "", label= "";
+
+    String TAG = "writeAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wite);
 
-        findview();
+        init();
         // findViewById 처리 함수
 
-        backbtn.setOnClickListener(this);
-        checkbtn.setOnClickListener(this);
-        s_txt.setOnClickListener(this);
-        e_txt.setOnClickListener(this);
-        fap.setOnClickListener(this);
-        start_pick.setOnClickListener(this);
-        end_pick.setOnClickListener(this);
+
 
 
     }
+
+    public void init(){
+        backbtn = findViewById(R.id.back_btn);
+        checkbtn = findViewById(R.id.check_btn);
+        titleEdit = findViewById(R.id.title_Edit);
+        desEdit = findViewById(R.id.description_Edit);
+        select_date = findViewById(R.id.select_Date);
+        s_date = findViewById(R.id.start_date);
+        e_date = findViewById(R.id.end_date);
+        fap = findViewById(R.id.fab_button);
+
+        backbtn.setOnClickListener(this);
+        checkbtn.setOnClickListener(this);
+        fap.setOnClickListener(this);
+        select_date.setOnClickListener(this);
+    }
+
 
     @Override
     protected void onStart() {
@@ -68,25 +77,14 @@ public class writeActivity extends AppCompatActivity implements View.OnClickList
         long now = System.currentTimeMillis();
         Date date = new Date(now);
         SimpleDateFormat sp = new SimpleDateFormat("yyyy/MM/dd");
-        stime = sp.format(date);
-        s_date.setText(stime);
-        e_date.setText(stime);
+        startDate = sp.format(date);
+        endDate = startDate;
+        s_date.setText(startDate);
+        e_date.setText(endDate);
+
 
     }
 
-    public void findview(){
-        backbtn = findViewById(R.id.back_btn);
-        checkbtn = findViewById(R.id.check_btn);
-        titleEdit = findViewById(R.id.title_Edit);
-        desEdit = findViewById(R.id.description_Edit);
-        start_pick = findViewById(R.id.start_pick);
-        end_pick = findViewById(R.id.end_pick);
-        s_date = findViewById(R.id.start_date);
-        e_date = findViewById(R.id.end_date);
-        s_txt = findViewById(R.id.start_day_txt);
-        e_txt= findViewById(R.id.end_day_txt);
-        fap = findViewById(R.id.fab_button);
-    }
 
     @Override
     public void onClick(View v) {
@@ -100,25 +98,30 @@ public class writeActivity extends AppCompatActivity implements View.OnClickList
             }
         }
         //제목, 입력되어있는지 확인 (제목만 작성 되어 있어도 괜찮음)
-        else if (v.getId() == R.id.start_date || v.getId() == R.id.start_day_txt) return;
-        else if (v.getId() == R.id.start_pick || v.getId() == R.id.end_pick){
-            showDatePickerDialog(v);
-        }
-        // 날짜를 띄워 줘야함 달력형식으로
+        else if (v.getId() == R.id.select_Date ) showDatePickerDialog();
+        // 달력으로
         else if (v.getId() == R.id.fab_button); return;
         // 사진 선택창 띄워줘야함
     }
 
-    public void showDatePickerDialog(View view){
-        //MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+    public void showDatePickerDialog(){
         MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
         builder.setTitleText("날짜를 선택하세요");
-        MaterialDatePicker materialDatePicker = builder.build();
+        final MaterialDatePicker materialDatePicker = builder.build();
         materialDatePicker.show(getSupportFragmentManager(), "태그");
 
-
-        /*DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");*/
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long,Long>>() {
+            @Override
+            public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                TimeZone timeZone = TimeZone.getDefault();
+                int offsetFromUTC = timeZone.getOffset(new Date().getTime()) * -1;
+                SimpleDateFormat spd = new SimpleDateFormat("yyyy/MM/dd");
+                startDate = spd.format(new Date(selection.first + offsetFromUTC));
+                endDate = spd.format(new Date(selection.second + offsetFromUTC));
+                Log.d(TAG, "start: "+ startDate);
+                Log.d(TAG, "end: " + endDate);
+            }
+        });
 
     }
 
